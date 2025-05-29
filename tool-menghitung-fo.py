2f23e2fde387856a1e52b34f3e5b97442b77f69c
r2bapp <rretortbersama@gmail.com>
2025-05-29 15:03:22 +0700

    Update tool-menghitung-fo.py

--- tool-menghitung-fo.py
+++ tool-menghitung-fo.py
@@ -151,8 +151,15 @@ img_buffer.seek(0)
 
 if st.button("📄 Ekspor ke PDF"):
     pdf = PDF()
-    pdf.add_metadata(nama_produk, tanggal_proses, nama_operator, nama_alat, nilai_f0, valid)
-# sekarang ini valid
+   pdf.add_metadata(
+    produk="",
+    tanggal="",
+    operator="Budi Santoso",
+    alat="Overpressure Retort",
+    f0=,
+    status=""
+)
+
     pdf_bytes = pdf.output(dest='S').encode('latin1')
     st.download_button("💾 Unduh PDF", data=pdf_bytes, file_name="laporan_validasi.pdf", mime="application/pdf")
 
